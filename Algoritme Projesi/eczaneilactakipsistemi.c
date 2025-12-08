c21c3998113ba93b028bb7bd50da3098f4078017
Suat Alper <suatalper2007@gmail.com>
2025-12-08 22:17:17 +0300

    Little Updates and Changes
    İ used new things like matris. And i think that my code cleaner right know

--- Algoritme Projesi/eczaneilactakipsistemi.c
+++ Algoritme Projesi/eczaneilactakipsistemi.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <windows.h>
 #include <stdbool.h>
+// BAS dizisi için indeksler (0, 1, 2)
+#define BARKOD 0
+#define ISIM   1
+#define SKT  2  
+
+// SSR dizisi için indeksler (0, 1, 2)
+#define STOK   0
+#define SOGUK  1
+#define RECETE 2
+
 //1.BAŞLA
 //2.ilaç ismini gir
 //3.İlaçtan kaç tane geldiğini gir
@@ -12,8 +22,57 @@
 //  2.İlaç stoğu baştaki stoğun %20 si kadarsa kullanıcıya uyarı ver
 //  3.HAYIRSA GİT 6.adım
 //6.BİTİR
+
+struct ilac
+{
+   // char barkod[20];
+   // char ad [50];
+    //char skt [15];
+
+    char BAS[100][3][50]; // Barkod , Ad , SKt
+    //Max 100 ilaç her ilacın 3 özelliği var
+    // 0: BArkod 1:AD 2:SKT
+    int SSR[100][3]; // Stok,Soguk zincir, recetürü
+    float fiyat[100];
+};
+tutorialFunc(){
+
+    printf("Burda nasıl yapılcağını anlat");
+}
+secim1İlacgiris(){
+    
+}
 void secim1(){
-    printf("İlaç Stokları Bölümüne HOŞGELDİNİZ !"); 
+    printf("İlaç Stokları Bölümüne HOŞGELDİNİZ ! \n"); 
+    int tutorial = 0;
+    printf("Merhaba daha önceden programı kullanmışmıydınız ? \n ");
+    printf("Kullandıysanız 1 'e basınız kullanmadıysanız ve Yardıma ihtiyacınız varsa 2'ye basınız\n");
+    scanf("%d",&tutorial);
+    if (tutorial == 1)
+    {
+      secim1İlacgiris();
+    }
+    else if (tutorial == 2)
+    {
+        tutorialFunc();
+    }
+    else
+    {
+        printf("lütfen geçerli bir değer giriniz");
+        if (tutorial == 1)
+        {
+            secim1İlacgiris();
+        }
+        else if (tutorial == 2)
+        {
+            tutorialFunc();
+        }
+        else
+        {
+            printf("lütfen geçerli bir değer giriniz");
+        }
+        
+    }
 }
 void secim2 (){
     printf("İlaç girişi kısmına HOŞGELDİNİZ");
@@ -48,18 +107,6 @@ void menu (){
         }
     }
 }
-
-struct ilac
-{
-    char barkod[20];
-    char ad [50];
-    int stok ;
-    float fiyat;
-    char skt [15];
-    int sogukzincir;
-    int recetetur;
-};
-
 int main() {
     SetConsoleOutputCP(65001);
     menu();
